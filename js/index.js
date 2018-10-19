fc15fde300f2c1bb754bbe0f794ba79d61fe9697
https://github.com/a641334373/TanHao.git <>
2018-10-19 16:58:34 +0800

    创建国新文件

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#carousel-example-generic').hammer().on('swipeleft', function(){
+			  $(this).carousel('next');
+			});
+			
+			$('#carousel-example-generic').hammer().on('swiperight', function(){
+			  $(this).carousel('prev');
+			});
+			
+$('#carousel-example-generic').hammer().on('swipeleft', function(){
+			$(this).carousel('next');
+		});
+$('#carousel-example-generic').hammer().on('swiperight', function(){
+	$(this).carousel('prev');
+});
